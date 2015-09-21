ff35122461a6f7864f50a8b3fbb0249407ebfc3e
Gilles Dubois <gillesdubois@users.noreply.github.com>
2015-09-21 14:23:10 +0200

    Finalize .js functions

--- Render/js/image.js
+++ Render/js/image.js
@@ -2,3 +2,8 @@ function toggleImage(id) {
     var lImage = document.getElementById(id);
     lImage.style.display = (lImage.style.display == "table") ? "show" : "table";
 }
+
+function hideImage(id) {
+    var lImage = document.getElementById(id);
+    lImage.style.display = (lImage.style.display == "table") ? "none" : "table";
+}
